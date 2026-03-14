85c73bf74e12b59b82bb672f0c801ac331fd665f
Randdy Fernando <randdyfernando@gmail.com>
2026-03-13 22:36:12 -0300

    Update supabase.ts with default key

--- auditor-pro-web/lib/supabase.ts
+++ auditor-pro-web/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL || 'https://dffhvutwwqeumyikinkc.supabase.co'
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJvbGUiOiJzZXJ2aWNlX2tleSIsImV4cCI6MTc3NDk4MjI4OX0.8HGJMhJpZDZN_2YNQeR8oJF8sFxylpSj0XQ7PfB3nVU'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  process.env.SUPABASE_URL || 'https://dffhvutwwqeumyikinkc.supabase.co',
+  process.env.SUPABASE_SERVICE_KEY || process.env.SUPABASE_SERVICE_ROLE_KEY || 'lJ3Z7dDp8PoDTWnZ'
+)
